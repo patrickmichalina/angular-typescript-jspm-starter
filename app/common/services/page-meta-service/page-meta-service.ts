d85c323336001bbffad788bde5986e9cec2c55f9
Patrick <patrickmichalina@mac.com>
2015-09-11 09:16:09 -0700

    feat(page title service)

--- app/common/services/page-meta-service/page-meta-service.ts
+++ app/common/services/page-meta-service/page-meta-service.ts
@@ -3,14 +3,33 @@ import {IPageMetaService} from './page-meta-service-interface'
 class PageMetaService implements IPageMetaService {
     private metaDescription: string;
     private metaKeywords: string;
-
-
-	constructor() {
-
+	
+	getMetaDescription(): string {
+		return this.metaDescription;
 	}
 	
+    setMetaDescription(newMetaDescription: string): void {
+		this.metaDescription = newMetaDescription;
+	}
 	
-
+    getMetaKeywords(): string {
+		return this.metaKeywords;
+	}
+	
+    setMetaKeywords(newKeywords: Array<string>): void {
+		for (var key in newKeywords) {
+            if (this.metaKeywords === '') {
+                this.metaKeywords += newKeywords[key];
+            } else {
+                this.metaKeywords += ', ' + newKeywords[key];
+            }
+        }
+	}
+	
+    reset(): void {
+		this.metaDescription = '';
+        this.metaKeywords = '';
+	}
 }
 
 export {PageMetaService}
