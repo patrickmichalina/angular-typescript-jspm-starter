17c94bcc59c29b4d23baddcbaa7e0b61624b5d49
Patrick <patrickmichalina@mac.com>
2015-09-11 21:21:07 -0700

    feat(removed ngAnimate)

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 /// <reference path="../typings/tsd.d.ts" />
 import {module} from "angular";
 import 'angular-ui-router';
-import 'angular-animate';
 import {Routes} from './layout/routes';
 import {LocationProviderConfig, ExceptionHandlerConfig} from './config/config';
 import {ComponentModule} from './components/components';
@@ -10,8 +9,7 @@ import {CommonModule} from './common/common';
 let Module = module("app", [
 	ComponentModule.name,
     CommonModule.name,
-	'ui.router',
-    'ngAnimate'
+	'ui.router'
 ]);
 
 // configure
